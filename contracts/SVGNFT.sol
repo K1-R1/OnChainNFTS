08f93dd111ab0eca2544c7d8441223eaaba7976e
K1-R1 <kyranrawlinson@gmail.com>
2022-02-22 18:42:55 +0000

    Updated contracts/SVGNFT.sol; added formatTokenURI function

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -15,6 +15,7 @@ contract SVGNFT is ERC721URIStorage {
     function create(string memory svg) public {
         _safeMint(msg.sender, tokenCounter);
         // imgURI
+        string memory imageURI = svgToImageURI(svg);
         // tokenURI
         tokenCounter = tokenCounter + 1;
     }
@@ -32,5 +33,29 @@ contract SVGNFT is ERC721URIStorage {
             abi.encodePacked(baseURL, svgBase64Encoded)
         );
         return imageURI;
+    }
+
+    function formatTokenURI(string memory imageURI)
+        public
+        pure
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                "SVG NFT",
+                                '", "description":"An NFT based on SVG", "attributes":"", "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
