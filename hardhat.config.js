f6442b34a321924fc84e6161105a55bb002b43d8
K1-R1 <kyranrawlinson@gmail.com>
2022-02-22 19:19:02 +0000

    Updated hardhat.config.js; added setup for deployments

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    // rinkeby:{
+    //   accounts: {}
+    // }
+  },
+
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  }
 };
