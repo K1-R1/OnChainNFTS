76ac985295c1291641b4b107768d1778c816c0e1
K1-R1 <kyranrawlinson@gmail.com>
2022-02-22 18:46:48 +0000

    Updated contracts/SVGNFT.sol; refactored function inputs

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -12,22 +12,22 @@ contract SVGNFT is ERC721URIStorage {
         tokenCounter = 0;
     }
 
-    function create(string memory svg) public {
+    function create(string memory _svg) public {
         _safeMint(msg.sender, tokenCounter);
-        string memory imageURI = svgToImageURI(svg);
+        string memory imageURI = svgToImageURI(_svg);
         string memory tokenURI = formatTokenURI(imageURI);
         _setTokenURI(tokenCounter, tokenURI);
         tokenCounter = tokenCounter + 1;
     }
 
-    function svgToImageURI(string memory svg)
+    function svgToImageURI(string memory _svg)
         public
         pure
         returns (string memory)
     {
         string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(
-            bytes(string(abi.encodePacked(svg)))
+            bytes(string(abi.encodePacked(_svg)))
         );
         string memory imageURI = string(
             abi.encodePacked(baseURL, svgBase64Encoded)
@@ -35,7 +35,7 @@ contract SVGNFT is ERC721URIStorage {
         return imageURI;
     }
 
-    function formatTokenURI(string memory imageURI)
+    function formatTokenURI(string memory _imageURI)
         public
         pure
         returns (string memory)
@@ -50,7 +50,7 @@ contract SVGNFT is ERC721URIStorage {
                                 '{"name":"',
                                 "SVG NFT",
                                 '", "description":"An NFT based on SVG", "attributes":"", "image":"',
-                                imageURI,
+                                _imageURI,
                                 '"}'
                             )
                         )
