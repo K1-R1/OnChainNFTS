24fb4c6fd48c9dc656df4be6c4e13aa68e3dd3bd
K1-R1 <kyranrawlinson@gmail.com>
2022-02-22 18:12:01 +0000

    Added basic contract setup for SVGNFT

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -3,3 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract SVGNFT is ERC721URIStorage {
+    constructor() ERC721("SVG NFT", "svgNFT") {}
+}
