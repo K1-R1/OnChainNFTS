4cba58bc8ce0b953ed457a50c63207e46b63d209
K1-R1 <kyranrawlinson@gmail.com>
2022-02-22 18:29:28 +0000

    Updated contracts/SVGNFT.sol; with svgToImageURI function

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "base64-sol/Base64.sol";
 
 contract SVGNFT is ERC721URIStorage {
     uint256 public tokenCounter;
@@ -16,5 +17,20 @@ contract SVGNFT is ERC721URIStorage {
         // imgURI
         // tokenURI
         tokenCounter = tokenCounter + 1;
+    }
+
+    function svgToImageURI(string memory svg)
+        public
+        pure
+        returns (string memory)
+    {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        string memory imageURI = string(
+            abi.encodePacked(baseURL, svgBase64Encoded)
+        );
+        return imageURI;
     }
 }
