de49f772464cdf3bf39bc0d4f5336f26117893bd
K1-R1 <kyranrawlinson@gmail.com>
2022-02-22 23:14:02 +0000

    Updated contracts/RandomSVGNFT.sol; updated with create function

--- contracts/RandomSVGNFT.sol
+++ contracts/RandomSVGNFT.sol
@@ -8,6 +8,12 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 contract RandomSVGNFT is ERC721URIStorage, VRFConsumerBase {
     bytes32 public keyHash;
     uint256 public fee;
+    uint256 public tokenCounter;
+
+    mapping(bytes32 => address) public requestIdToSender;
+    mapping(bytes32 => uint256) public requestIdToTokenId;
+
+    event RequestedRandomSVG(bytes32 indexed requestId, uint256 tokenId);
 
     constructor(
         address _VRFCoordinator,
@@ -20,5 +26,24 @@ contract RandomSVGNFT is ERC721URIStorage, VRFConsumerBase {
     {
         keyHash = _keyhash;
         fee = _fee;
+        tokenCounter = 0;
     }
+
+    function create() public returns (bytes32 requestId) {
+        requestId = requestRandomness(keyHash, fee);
+        requestIdToSender[requestId] = msg.sender;
+        uint256 tokenId = tokenCounter;
+        requestIdToTokenId[requestId] = tokenId;
+        tokenCounter = tokenCounter + 1;
+        emit RequestedRandomSVG(requestId, tokenId);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        randomResult = _randomness;
+    }
+
+    function finishMint() public {}
 }
