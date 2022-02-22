06d9ee907b6f1cf4182e1fcb19979f43afb3996a
K1-R1 <kyranrawlinson@gmail.com>
2022-02-22 23:53:49 +0000

    Updated contracts/RandomSVGNFT.sol; updated with uint2str function

--- contracts/RandomSVGNFT.sol
+++ contracts/RandomSVGNFT.sol
@@ -117,4 +117,30 @@ contract RandomSVGNFT is ERC721URIStorage, VRFConsumerBase {
         }
         finalSVG = string(abi.encodePacked(finalSVG, "</svg>"));
     }
+
+    function uint2str(uint256 _i)
+        internal
+        pure
+        returns (string memory _uintAsString)
+    {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len;
+        while (_i != 0) {
+            k = k - 1;
+            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
 }
