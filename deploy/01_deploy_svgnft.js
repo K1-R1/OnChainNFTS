8f5539abefdc90fb8205837fc3667e1be954a228
K1-R1 <kyranrawlinson@gmail.com>
2022-02-22 19:41:34 +0000

    Updated deploy/01_deploy_svgnft.js; added deployment functionality

--- deploy/01_deploy_svgnft.js
+++ deploy/01_deploy_svgnft.js
@@ -1,3 +1,6 @@
+const fs = require("fs")
+let { networkConfig } = require("../helper-hardhat-config")
+
 module.exports = async ({
     getNamedAccounts,
     deployments,
@@ -7,10 +10,23 @@ module.exports = async ({
     const { deployer } = await getNamedAccounts()
     const chainId = await getChainId()
 
-    log("--------------------")
+    log('\n--------------------')
     const SVGNFT = await deploy("SVGNFT", {
         from: deployer,
         log: true
     })
     log(`SVGNFT contract deployed to ${SVGNFT.address}`)
+
+    let filepath = "./img/circle.svg"
+    let svg = fs.readFileSync(filepath, { encoding: "utf8" })
+
+    const SVGNFTContract = await ethers.getContractFactory("SVGNFT")
+
+    const accounts = await hre.ethers.getSigners()
+    const signer = accounts[0]
+
+    const svgNFT = new ethers.Contract(SVGNFT.address, SVGNFTContract.interface, signer)
+
+    const networkName = networkConfig[chainId]['name']
+    log(`Verify with:\nnpx hardhat verify --network ${networkName} ${svgNFT.address}`)
 }
