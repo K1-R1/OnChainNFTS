fe8948429fb7d2caab0236c77c2d50013b8bdd24
K1-R1 <kyranrawlinson@gmail.com>
2022-02-22 18:16:12 +0000

    Updated SVGNFT.sol; added added create function setup

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -5,5 +5,16 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract SVGNFT is ERC721URIStorage {
-    constructor() ERC721("SVG NFT", "svgNFT") {}
+    uint256 public tokenCounter;
+
+    constructor() ERC721("SVG NFT", "svgNFT") {
+        tokenCounter = 0;
+    }
+
+    function create(string memory svg) public {
+        _safeMint(msg.sender, tokenCounter);
+        // imgURI
+        // tokenURI
+        tokenCounter = tokenCounter + 1;
+    }
 }
