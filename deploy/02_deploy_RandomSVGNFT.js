62d2a900aaff338dae7b978a0894618f0dfae3b3
K1-R1 <kyranrawlinson@gmail.com>
2022-02-23 23:46:53 +0000

    Updated deploy/02_deploy_RandomSVGNFT.js; added RandomSVGNFT link funding functionality

--- deploy/02_deploy_RandomSVGNFT.js
+++ deploy/02_deploy_RandomSVGNFT.js
@@ -1,3 +1,4 @@
+const { ethers } = require('ethers')
 let { networkConfig } = require('../helper-hardhat-config')
 
 module.exports = async ({
@@ -9,6 +10,7 @@ module.exports = async ({
     const { deployer } = await getNamedAccounts()
     const chainId = await getChainId()
 
+    // deploy RandomSVGNFT
     let linkTokenAddress
     let vrfCoordinatorAddress
     if (chainId == 31337) {
@@ -29,6 +31,18 @@ module.exports = async ({
         args: args,
         log: true
     })
+    const networkName = networkConfig[chainId]['name']
+    log(`Verify with:\n npx hardhat verify --network ${networkName} ${RandomSVGNFT.address} ${args.toString().replace(/,/g, " ")}`)
+
+    //Fund RandomSVGNFT with LINK
+    const linkTokenContract = await ethers.getContractFactory('LinkToken')
+    const accounts = await hre.ethers.getSigners()
+    const signer = accounts[0]
+    const linkToken = new ethers.Contract(linkTokenAddress, linkTokenContract.interface, signer)
+    const tx = await linkToken.transfer(RandomSVGNFT.address, fee)
+    await tx.wait(1)
+
+
 }
 
 module.exports.tags = ['all', 'rsvg']
