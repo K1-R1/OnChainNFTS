39252b529730aaa05ed8599ddd5bc92298b97e36
K1-R1 <kyranrawlinson@gmail.com>
2022-02-23 22:04:14 +0000

    Added deploy/00_deploy_mocks.js with basic setup

---
+++ deploy/00_deploy_mocks.js
@@ -0,0 +1,9 @@
+module.exports = async ({
+    getNamedAccounts,
+    deployments,
+    getChainId
+}) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = await getChainId()
+}
