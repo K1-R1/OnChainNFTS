06db3cf4ff6396847aaac38ec7d83c3bb3201335
K1-R1 <kyranrawlinson@gmail.com>
2022-02-23 22:23:43 +0000

    Updated deploy/02_deploy_RandomSVGNFT.js; with local deployment

--- deploy/02_deploy_RandomSVGNFT.js
+++ deploy/02_deploy_RandomSVGNFT.js
@@ -6,4 +6,14 @@ module.exports = async ({
     const { deploy, get, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = await getChainId()
+
+    let linkTokenAddress, vrfCoordinatorAddress
+    if (chainId == 313370) {
+        let linkToken = await get('LinkToken')
+        linkTokenAddress = linkToken.address
+        let vrfCoordinatorMock = await get('VRFCoordinatorMock')
+        vrfCoordinatorAddress = vrfCoordinatorMock.address
+    } else {
+
+    }
 }
