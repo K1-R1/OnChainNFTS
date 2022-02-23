83a10d79a8a5a83c2d5ebddd7632e739a820107b
K1-R1 <kyranrawlinson@gmail.com>
2022-02-23 22:50:48 +0000

    Updated deploy/02_deploy_RandomSVGNFT.js; added RandomSVGNFT deployment functionality

--- deploy/02_deploy_RandomSVGNFT.js
+++ deploy/02_deploy_RandomSVGNFT.js
@@ -9,17 +9,26 @@ module.exports = async ({
     const { deployer } = await getNamedAccounts()
     const chainId = await getChainId()
 
-    let linkTokenAddress, vrfCoordinatorAddress
-    if (chainId == 313370) {
+    let linkTokenAddress
+    let vrfCoordinatorAddress
+    if (chainId == 31337) {
         let linkToken = await get('LinkToken')
+        let VRFCoordinatorMock = await get('VRFCoordinatorMock')
         linkTokenAddress = linkToken.address
-        let vrfCoordinatorMock = await get('VRFCoordinatorMock')
-        vrfCoordinatorAddress = vrfCoordinatorMock.address
+        vrfCoordinatorAddress = VRFCoordinatorMock.address
     } else {
         linkTokenAddress = networkConfig[chainId]['linkToken']
         vrfCoordinatorAddress = networkConfig[chainId]['vrfCoordinator']
     }
-
     const keyHash = networkConfig[chainId]['keyHash']
     const fee = networkConfig[chainId]['fee']
+    args = [vrfCoordinatorAddress, linkTokenAddress, keyHash, fee]
+    log("----------------------------------------------------")
+    const RandomSVGNFT = await deploy('RandomSVGNFT', {
+        from: deployer,
+        args: args,
+        log: true
+    })
 }
+
+module.exports.tags = ['all', 'rsvg']
